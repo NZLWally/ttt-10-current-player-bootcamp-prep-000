08613c56de5a207155ed008628d4ce351e254b65
James Wallace <4538317+nzlwally@users.noreply.github.com>
2020-06-15 07:23:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   turns = 0
   board.each do |element|
-    turns += 1 unless element
+    turns += 1 unless element == " " || element == ""
   end
-  
+  turns
 end
 
 def current_player
